ae64534a73a3704e989a24ebf04335f395765029
Jonathan Brimer <jonathan@counteragent.com>
2017-07-03 11:51:08 -0400

    Init, kind of
    Changed to custom compiled version of bootstrap
    Updated version number to 0.1.7

---
+++ README.md
@@ -0,0 +1,25 @@
+# KH Camera Control
+PTZ Camera Control Software for Kingdom Halls (Electron App)
+
+This project is made for the PTZOptics line of IP Cameras.
+
+All the components of this project are built using web technologies. Javascript, HTML, CSS, etc. It can be used in it's basic form as Javascript, HTML, and CSS. However, it includes all the additional files necessary to create your own electron based app package.
+
+If you have any bugs or issues to report, please post them [here](https://github.com/counteragent/Camera-Control/issues).
+
+## Building the Electron App
+
+In order to build the Electron app you'll need the following:
+- GIT and Node.js: Just use [npm](http://npmjs.com), it includes both and is easiest
+- Electron: Follow the instructions found here: http://electron.atom.io/ or here: [Electron Quick Start](https://github.com/atom/electron-quick-start/blob/master/README.md#to-use)
+
+It's not required, but I found it much easier to use these packages as well:
+- [electron-packager](https://github.com/maxogden/electron-packager#installation)
+- [electron-builder](https://github.com/loopline-systems/electron-builder#install)
+
+When building on MacOS, you'll need to install [Wine](https://www.winehq.org/)
+- The easiest way is to install via [Homebrew](https://brew.sh/)
+ - `brew cask install xquartz`
+ - `brew install wine`
+
+Once you're ready to build the final app just use command `npm run dist`
---
+++ main.js
@@ -0,0 +1,94 @@
+const {app, BrowserWindow, Menu} = require('electron'); // Module to control application life.
+const path = require('path')
+const url = require('url')
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+let storage = require("./storage");
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+function createWindow () {
+
+	var lastWindowState = storage.get("lastWindowState");
+
+	// lastWindowState = null;
+	// storage.set("lastWindowState", null);
+
+	if (lastWindowState === null) {
+		lastWindowState = {
+			width: 400,
+			height: 430,
+			frame: false,
+			resizable: false
+		}
+	}
+
+	mainWindow = new BrowserWindow({
+		x: lastWindowState.x,
+		y: lastWindowState.y,
+		width: lastWindowState.width,
+		height: lastWindowState.height,
+		frame: false,
+		resizable: false
+	});
+
+	// and load the index.html of the app.
+	// mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+	// and load the index.html of the app.
+	mainWindow.loadURL(url.format({
+		pathname: path.join(__dirname, 'index.html'),
+		protocol: 'file:',
+		slashes: true
+	}));
+
+	// Open the DevTools.
+	// mainWindow.webContents.openDevTools();
+
+	mainWindow.on('close', function() {
+		var bounds = mainWindow.getBounds();
+		storage.set("lastWindowState", {
+			x: bounds.x,
+			y: bounds.y,
+			width: bounds.width,
+			height: bounds.height,
+			frame: false,
+			resizable: false
+		});
+	});
+
+	// Emitted when the window is closed.
+	mainWindow.on('closed', () => {
+		// Dereference the window object, usually you would store windows
+		// in an array if your app supports multi windows, this is the time
+		// when you should delete the corresponding element.
+		mainWindow = null;
+	});
+
+	// require('./menu');
+};
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+	// On macOS it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	/*if (process.platform !== 'darwin') {
+		app.quit();
+	}*/
+});
+
+app.on('activate', () => {
+	// On macOS it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (mainWindow === null) {
+		createWindow();
+	}
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
+
+app.on('ready', createWindow);
